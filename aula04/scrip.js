cf19147461fdd51f8659a58f8a8d7b9111fc7f21
unknown <leeschimit@gmail.com>
2019-11-07 22:00:15 -0300

    hoje foi foda

---
+++ aula04/scrip.js
@@ -0,0 +1,29 @@
+// Passo a Passo
+// // JEITO 2
+// // Clonar o repositório do seu projeto do Millos
+// // Criar pasta para js com arquivo .js
+// // Conectar o  no index.html
+// // Adicionar botão
+// // Chamar o botão e passar a função onclick direto no Java
+//     // DICA: guarde em uma variável "botao" o document com seletor do html
+
+
+
+// let botao = document.querySelector('.enviar');
+
+//     funtion mudarBotao (){
+//         botao.style.background.color = 'blue';
+//     }
+// }
+
+// botao.onclick = mudarBotao;
+
+// let botao = document.querySelector("button");
+
+// funtion tirarClass(){
+//     if(botao.classList.contains("enviar")){
+//         botao.classList.remove("enviar");
+//     }else{
+//         botao.classList.add("enviar")
+//     }
+// }

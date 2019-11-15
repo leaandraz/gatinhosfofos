4d68172eb5805fffdd3570a1a574276d87237416
unknown <leeschimit@gmail.com>
2019-11-14 22:04:18 -0300

    segundo commit

--- aula13/amarelinha/urls.py
+++ aula13/amarelinha/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from landing import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.mostrar_index),
+    path('sobre/', views.mostrar_sobre),
 ]
--- aula13/landing/views.py
+++ aula13/landing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mostrar_index(request):
+    return render(request, 'index.html')
+
+def mostrar_sobre(request):
+    return render(request, 'sobre.html')

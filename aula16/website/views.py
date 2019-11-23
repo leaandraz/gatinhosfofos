743980b7a71e108b1747276641d5c1a370a8d8d5
Hmoretti <leeschimit@gmail.com>
2019-11-22 22:07:08 -0300

    criar formularios

---
+++ aula16/website/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from website.forms import PedidoForm
+
+# Create your views here.
+def home(request):
+    nome = 'Groger'
+    idade = 18
+    lista_roupas = [
+        'Boné da lacoste',
+        'Boné da Cycl0ne',
+        'Cinto do Bátima',
+        'Toca da Medusa',
+        'Óculos Rift Zika'
+    ]
+
+    context = {
+        'roupas': lista_roupas,
+        'nome': nome,
+        'idade': idade
+    }
+    
+    return render(request, 'home.html', context)
+
+def produtos(request):
+    return render(request, 'produtos.html')
+
+def cadastro_pedido(request):
+    form = PedidoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {
+        'formulario':form
+    }
+    return render(request, 'pedido.html', context)

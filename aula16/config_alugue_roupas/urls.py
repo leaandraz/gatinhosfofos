86f15e2d09453d339f41525f49bbaa121322aabc
Roms2019 <romeniaishiyama@gmail.com>
2019-11-26 21:49:29 -0300

    criar renderização de MEDIA_ROOTS e MEDIA_URL

--- aula16/config_alugue_roupas/urls.py
+++ aula16/config_alugue_roupas/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from website import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('produtos/', views.produtos),
     path('cadastro/', views.cadastro_pedido)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
